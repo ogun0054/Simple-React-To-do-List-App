5ad24f3579fac8678d8b8b8348ea34ea0e8ff3fb
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-08-16 19:21:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// Import styled-components into our app
+import styled from "styled-components";
 
-function App() {
+// Using styled-components to style an h1
+const Header = styled.h1`
+  font-size: 32px;
+`;
+
+const tasks = [
+  { name: "Swab the deck", id: 1 },
+  { name: "Find treasure", id: 2 },
+  { name: "Set sail", id: 3 },
+];
+
+function Task({ name, onClick }) {
+  return <div onClick={onClick}> {name} </div>;
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header> 🏴‍☠️ Captain's Tasks </Header>
+      <p> Manage the daily jobs at sea </p>
+      <button onClick={() => alert("Adding task")}>Add Task</button>
+      {tasks.map(({ name, id }, i) => (
+        <Task key={`task-${id}`} name={name} />
+      ))}
     </div>
   );
 }
-
-export default App;
